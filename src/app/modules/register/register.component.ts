2d9c9662bc338600720bf0d17cc7836da65adbda
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-04-10 19:54:36 +0100

    first commit

--- src/app/modules/register/register.component.ts
+++ src/app/modules/register/register.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -7,22 +8,30 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  EXP = '^[a-zA-Z0-9.!#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$';
   types = [
     {value: 'CLIENT', viewValue: 'Client'},
     {value: 'COMMERCANT', viewValue: 'Commerçant'}
   ];
-   group = new FormGroup({
-     firstName : new FormControl('', [Validators.required]),
-     lastName: new FormControl('', [Validators.required]),
-     type : new FormControl('', [Validators.required]),
-     email: new FormControl('', [Validators.required]),
-     password: new FormControl('', [Validators.required, Validators.minLength(6)])
-   });
-  constructor() { }
+  group = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    type: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.pattern(this.EXP)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-   send = () => {
-    console.log(this.group.get('firstName').value);
-   }
+
+  send = () => {
+    if (this.group.get('firstName').invalid) {
+      Swal.fire('Prénom invalide ', '', 'error');
+      return;
+    }
+
+  };
 }
