a893515f3104fce9bad26382dc1d48f86ddfac23
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-08 15:16:23 +0100

    first  commit

--- src/app/modules/products/products-routing.module.ts
+++ src/app/modules/products/products-routing.module.ts
@@ -1,12 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ProductsComponent} from './products.component';
+import {ProductsAddComponent} from './products-add/products-add.component';
+import {ProductsEditComponent} from './products-edit/products-edit.component';
+import {ProductsListComponent} from './products-list/products-list.component';
+import {ProductsDetailsComponent} from './products-details/products-details.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductsComponent,
+    children: [
+      {path: 'add-products', component: ProductsAddComponent},
+      {path: 'edit-products', component: ProductsEditComponent},
+      {path: 'list-products', component: ProductsListComponent},
+      {path: 'details-products', component: ProductsDetailsComponent},
+    ]
+  }
+];
 
-
+//      {path: '', loadChildren: () => import('./drivers/drivers.module').then(m => m.DriversModule)},
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class ProductsRoutingModule { }
+export class ProductsRoutingModule {
+}
